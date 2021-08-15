abddb031017340919a30de5d8ca5d45c3fefbd9d
Tom Dowling <tom@digital-space.io>
2021-08-15 04:39:27 +0100

    Update: Adding the basic header links

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,8 +1,27 @@
-const Header = () => {    
+import Link from 'next/link';
+
+const Header = () => {
     return (
         <header>
             <div className="max-w-screen-md mx-auto">
                 <h1>Test</h1>
+
+                <ul className="flex flex-wrap text-xl">
+                    <li className="mr-6">
+                        <Link href="#about">
+                            <a className="text-gray-700 border-none hover:text-gray-900">
+                                About
+                            </a>
+                        </Link>
+                    </li>
+                    <li className="mr-6">
+                        <Link href="#contact">
+                            <a className="text-gray-700 border-none hover:text-gray-900">
+                                Contact
+                            </a>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </header>
     );
