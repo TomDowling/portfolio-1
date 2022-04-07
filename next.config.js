90d811098252802dfe1932fce4450a69269c9241
Tom Dowling <thomas.dowling@spikeglobal.com>
2022-04-07 14:18:30 +0100

    INIT: New Next.js Install

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-// next.config.js
-const withCSS = require('@zeit/next-css');
-const withSass = require("@zeit/next-sass");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = withCSS(withSass({
-    webpack (config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000
-          }
-        }
-      })
-  
-      return config
-    }
-  }))
+module.exports = nextConfig
