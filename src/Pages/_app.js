19ec145cf6cc681b484f4f96a4d2250431030c98
Tom Dowling <thomas.dowling@spikeglobal.com>
2022-04-07 15:07:25 +0100

    NEW: Adding SASS back to the project

--- src/Pages/_app.js
+++ src/Pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+// import '../../styles/globals.css'
+import styles from "../Styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
